10afe2789e28ec4357fa4f755fa47234681d8f40
Mu <huimin_mu1@baylor.edu>
2021-07-31 14:29:31 -0500

    Add methods to calculate net force

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -12,6 +12,9 @@ public class Planet {
 	public double mass;
 	public String imgFileName; 
 	// The name of the file that corresponds to the image that depicts the body
+	public static double gConstant = 6.67e-11;
+	// Hint: It is good practice to declare any constants as a ‘static final’ variable in your class, and to use that variable anytime you wish to use the constant.
+	// ??
 	public Planet (double xP, double yP, double xV,
               double yV, double m, String img) {
 		xxPos = xP;
@@ -31,4 +34,49 @@ public class Planet {
 		imgFileName = b.imgFileName;
 	}
 	// Your Planet class should NOT have a main method, because we’ll never run the Body class directly (i.e. we will never run java Body). Also, all methods should be non-static.
+	public double calcDistance (Planet b) {
+		double dx = b.xxPos - this.xxPos;
+		double dy = b.yyPos - this.yyPos;
+		double rSquare = dx * dx + dy * dy; 
+		// Hint: In Java, there is no built in operator that does squaring or exponentiation. We recommend simply multiplying a symbol by itself instead of using Math.pow, 
+		return Math.sqrt(rSquare);
+	}
+	public double calcForceExertedBy (Planet b) {
+		double rSquare = this.calcDistance(b) * this.calcDistance(b);
+		return gConstant * this.mass * b.mass / rSquare;
+	}
+	public double calcForceExertedByX (Planet b) {
+		double dx = b.xxPos - this.xxPos;
+		return calcForceExertedBy (b) * dx / this.calcDistance(b);
+	}
+	public double calcForceExertedByY (Planet b) {
+		double dy = b.yyPos - this.yyPos;
+		return calcForceExertedBy (b) * dy / this.calcDistance(b);
+	}
+	public double calcNetForceExertedByX (Planet[] allPlanets) {
+		/**
+		 * Take in an array of Planets and calculate the net X force 
+		 * exerted by all planets in that array upon the current Planet
+		 */
+		double netForceX = 0;
+		for (Planet p : allPlanets) {
+			if (!this.equals(p)) {
+				netForceX += this.calcForceExertedByX(p);
+			}
+		}
+		return netForceX;
+	}
+	public double calcNetForceExertedByY (Planet[] allPlanets) {
+		/**
+		 * Take in an array of Planets and calculate the net X force 
+		 * exerted by all planets in that array upon the current Planet
+		 */
+		double netForceY = 0;
+		for (Planet p : allPlanets) {
+			if (!this.equals(p)) {
+				netForceY += this.calcForceExertedByY(p);
+			}
+		}
+		return netForceY;
+	}
 }
